ba9eac21a13660d4c940a198e6330610c3ccca2c
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-10-01 19:39:22 -0500

    Getting list input, fixed test

--- venv/fun_with_collections/basic_list.py
+++ venv/fun_with_collections/basic_list.py
@@ -1,11 +1,21 @@
 
 def make_list():
-    pass
+    input = []
+    for i in range(3):
+        input.append(get_input())
+    return input
+
 
 def get_input():
-    pass
+    new_num = 0
+    try:
+        new_num = int(input("Please enter a number: "))
+    except:
+        print("Error, please enter a number")
+    return new_num
 
 
 if __name__ == '__main__':
-    make_list()
-    get_input()
+    list = make_list()
+    for i in range(len(list)):
+        print(list[i])
--- venv/test_collections/test_basic_list.py
+++ venv/test_collections/test_basic_list.py
@@ -6,4 +6,4 @@ import fun_with_collections.basic_list as basic_list
 class TestList(unittest.TestCase):
     @patch('fun_with_collections.basic_list.get_input', return_value='3')
     def test_make_list(self, input):
-        self.assertEqual(basic_list.make_list(), [3,3,3,3])
+        self.assertEqual(basic_list.make_list(), [3,3,3])
