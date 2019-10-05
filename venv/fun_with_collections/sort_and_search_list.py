3678d5d15430a7e099bee942f76fc50974d53227
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-10-05 11:37:07 -0500

    Wrote code to search list, tests succeeding.

--- venv/fun_with_collections/sort_and_search_list.py
+++ venv/fun_with_collections/sort_and_search_list.py
@@ -1,5 +1,13 @@
 def sort_list():
-    pass
+    list = [1,2,4,5,3]
+    looking_for = int(input("What number are you looking for: "))
+    for i in list:
+        if int(i) == int(looking_for):
+            print("Found number at place " + str(list[i]))
+            return list[i]
+    print("Found no number in list matching " + str(looking_for))
+    return -1
+
 
 def search_list():
     pass
--- venv/test_collections/test_sort_and_search_list.py
+++ venv/test_collections/test_sort_and_search_list.py
@@ -4,13 +4,15 @@ import fun_with_collections.sort_and_search_list as sort_and_search_list
 
 
 class TestList(unittest.TestCase):
-    @patch('fun_with_collections.sort_and_search_list.search_list', return_value='3')
-    def test_search_list(self, input):
-        self.assertEqual(sort_and_search_list.search_list(), 3)
+    @patch('builtins.input', side_effect='3')
+    def test_search_list(self, mock_input):
+        result = sort_and_search_list.sort_list()
+        self.assertEqual(result, 5)
 
-    @patch('fun_with_collections.sort_and_search_list.search_list', return_value='3')
-    def test_search_list_not_found(self, input):
-        self.assertEqual(sort_and_search_list.search_list(), -1)
+    @patch('builtins.input', side_effect='6')
+    def test_search_list_not_found(self, mock_input):
+        result = sort_and_search_list.sort_list()
+        self.assertEqual(result, -1)
 
 if __name__ == '__main__':
     unittest.main()
