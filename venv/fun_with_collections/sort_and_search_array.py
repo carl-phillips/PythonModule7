991794f95dca22afa3f1f1ec38f213ff127e98ba
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-10-05 13:16:49 -0500

    Wrote code for sort_array, tests succeeding!

--- venv/fun_with_collections/sort_and_search_array.py
+++ venv/fun_with_collections/sort_and_search_array.py
@@ -4,9 +4,17 @@ def sort_array(array):
     pass
 
 def search_array():
-    pass
+    a = arr.array('i', [6,3,4,1,8,7])
+    looking_for = int(input("What number are you looking for: "))
+    try:
+        print("Found number at place " + str(a.index(looking_for) + 1))
+        return a.index(looking_for)
+    except ValueError:
+        print("Found no number in list matching " + str(looking_for))
+        return -1
 
 
 if __name__ == '__main__':
     search_array()
-
+    array = sort_array(arr.array('i', [3,5,7,2,1,4]))
+    print(array)
