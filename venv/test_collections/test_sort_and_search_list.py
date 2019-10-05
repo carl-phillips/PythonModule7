798e7e600b0d082717839b2e5105d1c1edd13a15
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-10-05 11:46:27 -0500

    Wrote failing test for sort

--- venv/test_collections/test_sort_and_search_list.py
+++ venv/test_collections/test_sort_and_search_list.py
@@ -6,13 +6,16 @@ import fun_with_collections.sort_and_search_list as sort_and_search_list
 class TestList(unittest.TestCase):
     @patch('builtins.input', side_effect='3')
     def test_search_list(self, mock_input):
-        result = sort_and_search_list.sort_list()
+        result = sort_and_search_list.search_list()
         self.assertEqual(result, 5)
 
     @patch('builtins.input', side_effect='6')
     def test_search_list_not_found(self, mock_input):
-        result = sort_and_search_list.sort_list()
+        result = sort_and_search_list.search_list()
         self.assertEqual(result, -1)
+
+    def test_sort_list(self):
+        self.assertEqual(sort_and_search_list.sort_list([2,1,3,4]), [1,2,3,4])
 
 if __name__ == '__main__':
     unittest.main()
