9d7143f2ef6956c60747a59e7534f7a3d271cd3e
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-10-05 13:23:11 -0500

    Wrote code for sort array, test succeeding.

--- venv/fun_with_collections/sort_and_search_array.py
+++ venv/fun_with_collections/sort_and_search_array.py
@@ -1,8 +1,17 @@
+"""Carl Phillips
+This program sorts an array given, and searches an array for a value the user provides
+"""
 import array as arr
 
 def sort_array(array):
-    pass
+    for i in range(len(array)):
+        for j in range(0, len(array)-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
 
+    return array.tolist()       #I decided to return a list isntead of an array,
+                                #this way it is easier to get values
+                                #I also decided to return so that we can more easily use this in the future
 def search_array():
     a = arr.array('i', [6,3,4,1,8,7])
     looking_for = int(input("What number are you looking for: "))
