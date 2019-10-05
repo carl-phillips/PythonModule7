fa9c062d3957ed5220a02d0da7359bf0da5ad270
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-10-05 11:49:37 -0500

    Wrote code for sort_list, successful tests.

--- venv/fun_with_collections/sort_and_search_list.py
+++ venv/fun_with_collections/sort_and_search_list.py
@@ -1,4 +1,15 @@
-def sort_list():
+def sort_list(list):
+    for num in range(len(list)-1,0,-1):
+        for i in range(num):
+            if list[i] > list[i+1]:
+                temp = list[i]
+                list[i] = list[i+1]
+                list[i+1] = temp
+
+    return list         #I decided to return this list so that we can reuse this in the future
+                        #instead of listing it here we can portion that part out depending on what we want to do with it
+
+def search_list():
     list = [1,2,4,5,3]
     looking_for = int(input("What number are you looking for: "))
     for i in list:
@@ -9,12 +20,7 @@ def sort_list():
     return -1
 
 
-def search_list():
-    pass
-
-def get_list():
-    pass
-
-
 if __name__ == '__main__':
-    sort_list()
+    search_list()
+    list = sort_list([3,5,7,2,1,4])
+    print(list)
